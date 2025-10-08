0747ff1df93bf87046041ce5bb171389b4550916
Harry <hariharasethi89@gmail.com>
2025-10-08 17:54:24 +0530

    update: on executing command exexuting func

--- tui/cmd/main.go
+++ tui/cmd/main.go
@@ -5,12 +5,27 @@ import (
 	"fmt"
 
 	"github.com/harry713j/p2p-share/tui/internal/command"
+	"github.com/harry713j/p2p-share/tui/internal/service"
+	"github.com/harry713j/p2p-share/tui/internal/util"
 )
 
 func main() {
-
 	flag.Parse()
-	fmt.Println(*command.Send)
-	fmt.Println(*command.Receive)
 
+	filePath := *command.Send
+	u := util.NewUtility()
+	port := u.GetDynamicPort()
+	err := service.SendFile(filePath, fmt.Sprint(port))
+
+	if err != nil {
+		fmt.Printf("File send failed: %v\n", err)
+		return
+	}
+
+	code := *command.Receive
+	err = service.ReceiveFile(code)
+
+	if err != nil {
+		fmt.Printf("File download failed: %v\n", err)
+	}
 }
